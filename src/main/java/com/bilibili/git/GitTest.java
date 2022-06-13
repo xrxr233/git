6f868bdbbb77815b99333e77528c8c1a54adeb8a
xuren <xuren18151310314@163.com>
2022-06-13 16:30:08 +0800

    hot-fix commit1

--- src/main/java/com/bilibili/git/GitTest.java
+++ src/main/java/com/bilibili/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git Git!");
         System.out.println("Hello Git Git Git!");
+        System.out.println("Hello Git Git Git Git!");
     }
 }
